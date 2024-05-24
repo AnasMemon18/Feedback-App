e38191640571dc77e626a570eda84b00e0c59342
AnasMemon18 <anasmemon7864@gmail.com>
2024-05-24 17:17:53 +0530

    final commit 1

--- src/components/CustomTable/CustomTable.jsx
+++ src/components/CustomTable/CustomTable.jsx
@@ -1,26 +1,58 @@
 import React from "react";
-import Button from "@mui/material/Button";
-
+import { styled } from "@mui/material/styles";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+import { Typography } from "@mui/material";
 
-const CustomTable = ( props ) => {
+const CustomTable = (props) => {
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#CAF4FF",
+      color: "black",
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
   return (
     <>
       <TableContainer className="table-container" component={Paper}>
-        <Table sx={{ minWidth: 650, mx:"auto" }} aria-label="simple table">
+        <Table sx={{ minWidth: 650, mx: "auto" }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell align="center"><b>First Name</b></TableCell>
-              <TableCell align="center"><b>Last Name</b></TableCell>
-              <TableCell align="center"><b>Email</b></TableCell>
-              <TableCell align="center"><b>Feedback</b></TableCell>
-              <TableCell align="left"><b>Actions</b></TableCell>
+              <StyledTableCell align="center">
+                <Typography style={{ fontWeight: "bold" }} variant="h6">
+                  First Name
+                </Typography>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <Typography style={{ fontWeight: "bold" }} variant="h6">
+                  Last Name
+                </Typography>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <Typography style={{ fontWeight: "bold" }} variant="h6">
+                  Email
+                </Typography>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <Typography style={{ fontWeight: "bold" }} variant="h6">
+                  Feedback
+                </Typography>
+              </StyledTableCell>
+              <StyledTableCell align="center">
+                <Typography style={{ fontWeight: "bold" }} variant="h6">
+                  Actions
+                </Typography>
+              </StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
@@ -29,25 +61,38 @@ const CustomTable = ( props ) => {
                 key={row.name}
                 sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
               >
-                <TableCell align="center">{row.firstName}</TableCell>
-                <TableCell align="center">{row.lastName}</TableCell>
-                <TableCell align="center">{row.email}</TableCell>
-                <TableCell align="center">{row.feedback}</TableCell>
+                <TableCell align="center">
+                  <Typography variant="body2">{row.firstName}</Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="body2">{row.lastName}</Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="body2">{row.email}</Typography>
+                </TableCell>
+                <TableCell align="center">
+                  <Typography variant="body2">
+                    {row.feedback}
+                  </Typography>
+                </TableCell>
 
-                <Button
-                  variant="outlined"
-                  color="success"
-                  onClick={() => props.updateIdHandler(row._id)}
-                >
-                  Update
-                </Button>
-                <Button
-                  variant="outlined"
-                  color="error"
-                  onClick={() => props.deleteHandler(row._id)}
-                >
-                  Delete
-                </Button>
+                <TableCell align="center">
+                  <IconButton
+                    aria-label="edit"
+                    color="primary"
+                    onClick={() => props.updateIdHandler(row._id)}
+                  >
+                    <BorderColorIcon />
+                  </IconButton>
+
+                  <IconButton
+                    aria-label="delete"
+                    color="error"
+                    onClick={() => props.deleteHandler(row._id)}
+                  >
+                    <DeleteIcon />
+                  </IconButton>
+                </TableCell>
               </TableRow>
             ))}
           </TableBody>
